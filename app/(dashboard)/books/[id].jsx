69dc410ea0d0b3e033c376545191251eabc82111
itspranit <pranitrathkanthiwar123@gmail.com>
2025-12-10 14:09:43 +0530

    added delete buttn

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,17 +1,69 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { router, useGlobalSearchParams, useLocalSearchParams } from 'expo-router'
+import { useEffect,useState } from 'react'
+import { useBooks } from '../../../hooks/useBooks'
+
 import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
 import ThemedView from '../../components/ThemedView'
 import ThemedCard from '../../components/ThemedCard'
+import ThemedLoader from '../../components/ThemedLoader'
 import ThemedButton from '../../components/ThemedButton'
+import { Color } from 'react-native/types_generated/Libraries/Animated/AnimatedExports'
+import { Colors } from '../../../constants/Colors'
 
 const BookDetails = () => {
-  return (
+    const [book,setBook] = useState(null)
+    const {id}=useGlobalSearchParams()//here inside the curly braces we can destructure can attr we wanr but it should match the file name inside square bracket ie [id]
+    const {fetchBookById} =useBooks()
+    useEffect(()=>{
+        async function loadBook(){
+            const bookData=await fetchBookById(id)
+            setBook(bookData)
+        }
+
+        loadBook()
+    },[id])
+
+   const handleDelete =async()=>{
+     await deleteBook(id)
+     setBook(null)
+     router.replace('/books')
+   }
+   
+    if(!book){
+        return(
+            <ThemedView safe={true} style={styles.container}>
+                <ThemedLoader />
+           </ThemedView>
+
+        )
+    }
+    return (
     <ThemedView safe={true} style={styles.container} >
-        <ThemedText>
-         Book details
+       <ThemedCard>
+       <ThemedText>
+            {book.title}
         </ThemedText>
+        <ThemedText>
+            written by {book.author}
+        </ThemedText>
+        <Spacer />
+        <ThemedText title={true}>
+         Book description
+        </ThemedText>
+        <Spacer height={10}/>
+        <ThemedText>
+            {book.description}
+        </ThemedText>
+       </ThemedCard>
+       <ThemedButton style={styles.delete}>
+        <ThemedButton style={styles.delete} onPress={handleDelete}></ThemedButton>
+        <Text style={{color:`#fff`,textAlign:'center'}}>
+            DELETE BOOK
+        </Text>
+       </ThemedButton>
         </ThemedView>
   )
 }
@@ -22,5 +74,17 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems:"stretch"
+    },
+    title:{
+        fontSize:22,
+        marginVertical:10,
+    },
+    card:{
+        margin:20
+    },
+    delete:{
+        marginTop:40,
+        backgroundColor:Colors.warning,
+        alignSelf:'center'
     }
 })
